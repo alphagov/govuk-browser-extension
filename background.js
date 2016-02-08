56241df6499a0600073fc05008bde01b0819e52f
Tijmen Brommet <tijmen@gmail.com>
2016-02-08 15:28:42 +0000

    Remove preview environment
    The "preview" env has been replaced with "integration".

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 // This script runs in the background in Chrome. It will make the small
 // GOV.UK logo visible in the URL bar whenever we're on a gov.uk page.
 function showIconForGovukPages(tabId, changeInfo, tab) {
-  if (tab.url.match(/gov\.uk/) || tab.url.match(/alphagov\.co\.uk/)) {
+  if (tab.url.match(/gov\.uk/)) {
     chrome.pageAction.show(tabId);
   }
 }
