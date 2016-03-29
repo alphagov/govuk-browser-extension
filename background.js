c8b4213777b1d86ef3aad8e505c7d4e39b08398c
Tijmen Brommet <tijmen@gmail.com>
2016-03-29 18:49:04 +0100

    Improve active/inactive chrome button
    As discussed in
    https://github.com/alphagov/govuk-toolkit-chrome/issues/5, Chrome now
    always displays the button for an extension on the right of the omnibar.
    
    This commit adds correctly sized icons (thanks @joelanman) so that the
    logo changes visibly between gov.uk/other pages and always looks crisp.

--- background.js
+++ background.js
@@ -1,8 +1,9 @@
-// This script runs in the background in Chrome. It will make the small
-// GOV.UK logo visible in the URL bar whenever we're on a gov.uk page.
+// This script runs in the background in Chrome. It will activate the small
+// greyed out GOV.UK logo in the Chrome menu bar whenever we're on a gov.uk page.
 function showIconForGovukPages(tabId, changeInfo, tab) {
   if (tab.url.match(/www\.gov\.uk/) || tab.url.match(/dev\.gov\.uk/) || tab.url.match(/.*publishing\.service\.gov\.uk/)) {
     chrome.pageAction.show(tabId);
+    chrome.pageAction.setIcon({tabId: tabId, path: { '19': 'icons/crown-logo-19-active.png', '38': 'icons/crown-logo-38-active.png' }});
   }
 }
 
