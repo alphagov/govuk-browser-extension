c99c63872fed9855fd0fe93d1e9abd91b6d76c09
Tijmen Brommet <tijmen@gmail.com>
2016-04-29 13:15:05 +0100

    Extract URLs with double slashes correctly
    Fixes #11.

--- spec/javascripts/popup_view_spec.js
+++ spec/javascripts/popup_view_spec.js
@@ -24,6 +24,12 @@ describe("PopupView", function() {
       expect(path).toBe("/browse/disabilities")
     })
 
+    it("returns the path for URLs with double slashes", function () {
+      var path = Popup.extractPath(stubLocation("https://www.gov.uk/api/content//browse/disabilities"))
+
+      expect(path).toBe("/browse/disabilities")
+    })
+
     it("returns the path for incoming-links pages", function () {
       var path = Popup.extractPath(stubLocation("https://www.gov.uk/api/incoming-links/alpha-taxonomy/4-nannies-and-au-pairs?types[]=topics"))
 
