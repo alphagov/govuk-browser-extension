2eac536690e69dceb3045dae07d6bf1b90eb463b
Tijmen Brommet <tijmen@gmail.com>
2016-05-09 22:43:54 +0100

    Allow switching to the National Archives
    The National Archives keeps a history of GOV.UK. This commit adds
    support for switching between the current site and the index page for
    the URL on http://webarchive.nationalarchives.gov.uk/.

--- src/menu/popup_view.js
+++ src/menu/popup_view.js
@@ -78,6 +78,8 @@ Popup.extractPath = function(location) {
 
   if (location.href.match(/api\/content/)) {
     extractedPath = location.pathname.replace('api/content/', '');
+  } else if (location.href.match(/nationalarchives.gov.uk/)) {
+    extractedPath = location.pathname.split('https://www.gov.uk')[1];
   } else if (location.href.match(/api\/incoming-links/)) {
     extractedPath = location.pathname.replace('api/incoming-links/', '');
   } else if (location.href.match(/search.json/)) {
@@ -103,13 +105,20 @@ Popup.generateContentLinks = function(location) {
     return [];
   }
 
+  var originHost = location.origin;
+
+  if (originHost == 'http://webarchive.nationalarchives.gov.uk') {
+    originHost = "https://www.gov.uk"
+  }
+
   var links = [
-    { name: "On GOV.UK", url: location.origin + path },
-    { name: "Content item (JSON)", url: location.origin + "/api/content" + path },
-    { name: "Incoming links (JSON)", url: location.origin + "/api/incoming-links" + path + '?types[]=mainstream_browse_pages&types[]=topics&types[]=organisations&types[]=alpha_taxons'},
-    { name: "View search data", url: location.origin + "/api/search.json?filter_link=" + path },
-    { name: "Info page (not always available)", url: location.origin + "/info" + path },
-    { name: "Content API (JSON, deprecated)", url: location.origin + "/api" + path + ".json" },
+    { name: "On GOV.UK", url: originHost + path },
+    { name: "Content item (JSON)", url: originHost + "/api/content" + path },
+    { name: "Incoming links (JSON)", url: originHost + "/api/incoming-links" + path + '?types[]=mainstream_browse_pages&types[]=topics&types[]=organisations&types[]=alpha_taxons'},
+    { name: "View search data", url: originHost + "/api/search.json?filter_link=" + path },
+    { name: "Info page (not always available)", url: originHost + "/info" + path },
+    { name: "Content API (JSON, deprecated)", url: originHost + "/api" + path + ".json" },
+    { name: "National Archives", url: "http://webarchive.nationalarchives.gov.uk/*/https://www.gov.uk" + path },
   ]
 
   return links.map(function (link) {
