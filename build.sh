0f12c95bfeff70dfc72beb956f1a183e377732cc
syed-ali-tw <syed.ali1@digital.cabinet-office.gov.uk>
2024-10-24 13:01:44 +0100

    Update build script to create separate build for chrome and firefox
    Chrome and firefox have disagreements when it comes to certain fiels
    in manifest.json
    1- Firefox allows background.scripts and chrome allows backround.worker
    2- Firefox requires browser_specific_settings.gecko id and chrome do not
    
    Given these two differences, we cant use the same manifest.json for builds
    for both the browser thus, we have created two separate manifest.json for each browser.

--- build.sh
+++ build.sh
@@ -3,5 +3,10 @@
 echo "Building package"
 version=$(jq -r '.version' < src/manifest.json)
 echo "Version is $version"
+rm -f manifest.json
 cd src
-zip --exclude .DS_Store -r ../build/govuk-browser-extension-$version.zip .
+jq -s 'add' common.json manifest_chrome.json > manifest.json
+zip --exclude=.DS_Store --exclude=manifest_chrome.json --exclude=manifest_firefox.json -r ../build/govuk-browser-extension-chrome-$version.zip .
+rm -f manifest.json
+jq -s 'add'  common.json manifest_firefox.json > manifest.json
+zip --exclude=.DS_Store --exclude=manifest_chrome.json --exclude=manifest_firefox.json -r ../build/govuk-browser-extension-firefox-$version.zip .
